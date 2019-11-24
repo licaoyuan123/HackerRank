212debb207d29dbd33b2ec5ef890390b669a89c8
Caoyuan Li <408271618@qq.com>
2019-11-24 12:00:06 +1100

    Create CountTriplets.py

---
+++ CountTriplets.py
@@ -0,0 +1,8 @@
+#From https://www.hackerrank.com/challenges/count-triplets-1/forum
+#Couple of hints:
+#Can be done in O(n) -> single pass through data
+#No division necessary and single multiplications by R are all that's needed
+#Using map(C++) or dict(Java, Python) is a must -> can be unordered map (saves O(logN))
+#Try to think forward when reading a value -> will this value form part of a triplet later?
+#No need to consider (R == 1) as a corner case
+#Very interesting problem and it took a while to think it through properly. If anyone is desperate for the code, drop me a message.
